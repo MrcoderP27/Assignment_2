857a7e8c6106e81f442eec95b1a378ef54859f10
Parth <parthpanjwani27@gamil.com>
2025-08-12 17:18:27 +0530

    completed

---
+++ Task_2.py
@@ -0,0 +1,8 @@
+# Initialize a variable to score the sum
+total_sum = 0
+
+# using for loop
+for i in range(1,51) :
+    total_sum += i #Adds each number to total_sum
+
+print("The sum of integers from 1 to 50 is: ",total_sum )
